bfcadd03476b43c2d1cce93679a681477319a004
marcoklein <marco.klein@hpe.com>
2016-10-21 17:28:49 +0200

    updated modules

--- public/modules.js
+++ public/modules.js
@@ -1,18 +1,55 @@
 
-
-
-
 /* global _, google */
+
+
+/**
+ * Modules build up the dashboard and contain logic for how to render data.
+ * 
+ * Client data is held locally in the TodoistData object.
+ * 
+ */
+
+
+/**
+ * Holds client data Modules can use.
+ * 
+ * @type type
+ */
+var TodoistData = {
+    /* All client data recieved by a sync request. */
+    sync: null,
+    /* All completed items and projects. May take long to load. */
+    completed: null
+};
+
+/**
+ * If you write a new module add it here so your module code gets executed.
+ * 
+ * Holds all available modules.
+ * 
+ * @type Array
+ */
+var AvailableModules = [
+    ProjectsAndItemsPieChart,
+    NumberOfItemsPerDayColumnChart
+];
+
+
+/**
+ * Each Module must override the render() method. Render() is called if the
+ * gets rendered.
+ */
+
 
 google.charts.load('current', {'packages': ['corechart', "bar"]});
 google.charts.setOnLoadCallback(drawChart);
-function drawChart() {
-    console.log("Post request");
-    $.post("/API/v7/sync", function (res) {
-        var todoistData = JSON.parse(res);
+
+ProjectsAndItemsPieChart = {
+    render: function () {
+        var todoistData = JSON.parse(TodoistData.sync);
 
         var dataArray = [['Task', 'Hours per Day']];
-        
+
         var itemCountArray = _.map(todoistData.projects, function (project) {
             return _.filter(todoistData.items, function (item) {
                 return item.project_id === project.id;
@@ -23,9 +60,7 @@ function drawChart() {
             dataArray.push([
                 todoistData.projects[i].name,
                 itemCountArray[i]
-            ]
-
-                    );
+            ]);
         }
 
         var data = google.visualization.arrayToDataTable(
@@ -38,39 +73,41 @@ function drawChart() {
         var chart = new google.visualization.PieChart(document.getElementById('piechart'));
 
         chart.draw(data, options);
-        
-        
-    });
-    
-    
-        console.log("Making completed request.");
-    $.post("/API/v7/completed/get_all", function (res) {
+    }
+};
+
+NumberOfItemsPerDayColumnChart = {
+    render: function () {
+        if (!TodoistData.completed) {
+            return; // no completed data
+        }
         console.log("Response completed request.");
-        var completedItems = JSON.parse(res);
-        
+
+        var completedItems = JSON.parse(TodoistData.completed);
+
         var weekdays = ["Mon", "Tue", "Wed", "Thu", "Fri", "Sat", "Sun"];
         var shortWeekdays = ["Mon", "Tue", "Wed", "Thu", "Fri", "Sat", "Sun"];
-        
-        
+
+
         var shortWeekdays = _.map(shortWeekdays, function (day) {
             return _.filter(completedItems, function (item) {
                 return item.completed_date.startsWith(day);
             }).length;
         });
-        
+
         var arrayData = [];
-        
+
         for (var i = 0; i < weekdays.length; i++) {
             arrayData.push([weekdays[i], shortWeekdays[i], "blue"]);
         }
-        
-        
+
+
         console.log(JSON.stringify(arrayData));
-        
-        
+
+
         var dataArray = $.merge([["Element", "Density", {role: "style"}]], arrayData);
         console.log(JSON.stringify(dataArray));
-        
+
         var data = google.visualization.arrayToDataTable(dataArray);
 
         var view = new google.visualization.DataView(data);
@@ -90,8 +127,31 @@ function drawChart() {
         };
         var chart = new google.visualization.ColumnChart(document.getElementById("chart_most_productive_day"));
         chart.draw(view, options);
+    }
+};
 
+function getTodoistData() {
+    $.post("/API/v7/sync", function (res) {
+        TodoistData.sync = JSON.parse(res);
+        renderDashboard();
+    });
     
+    $.post("/API/v7/completed/get_all", function (res) {
+        TodoistData.completed = JSON.parse(res);
+        renderDashboard();
     });
     
 }
+
+/**
+ * Goes through the array of available modules and calls render on them.
+ * 
+ * @returns {undefined}
+ */
+function renderDashboard() {
+    for (var i = 0; i < AvailableModules.length; i++) {
+        AvailableModules.render();
+    }
+}
+
+getTodoistData();
