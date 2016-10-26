eaaaecd1ccf19495b7cc7ac7ff3ffcdb265cec21
marcoklein <marco.klein@hpe.com>
2016-10-26 13:06:06 +0200

    Revert "only user is loaded"
    This reverts commit 9a1d85737adeb9135f1abe32cf224ccc0ac07239.

--- main.js
+++ main.js
@@ -65,7 +65,10 @@ app.get('/', function (req, res) {
         var todoist = new TodoistAPI(accessToken);
         
         
-        todoist.sync(["user"]).then(function (value) {
+        todoist.sync().then(function (value) {
+            console.log("Sync completed: " + value);
+            
+            console.log("Completed count: " + value.user.completed_count);
             
             // get user id
             var userId = value.user.id;
@@ -74,10 +77,10 @@ app.get('/', function (req, res) {
             req.session.user_id = userId;
             
             
-            //userCache.put(userId, "sync", value);
+            userCache.put(userId, "sync", value);
             
             
-            res.render("index.html", { user_name: value.user.full_name, completed_count: value.user.completed_count });
+            res.render("index.html", { todoist_data: value, todoist_data_string: JSON.stringify(value) });
         }).catch(function (error) {
             // could not make sync request
             // -> access token may be invalid
